21223bd458eb49e6947d45b4bcd4b916cba6d5d9
zBuLe <zbule@bluestar03.com>
2025-05-14 16:27:16 -0700

    style: add constant mode for GML syntax highlighting to support predefined constants

--- assets/js/gml.js
+++ assets/js/gml.js
@@ -2,11 +2,11 @@ function hljsDefineGML(hljs) {
   const gmlNumberMode = {
     className: 'number',
     variants: [
-      { begin: /\b0b[01]+/i },                                   // binary
-      { begin: /\b0x[\da-f]+/i },                               // hex
-      { begin: /\$[0-9a-f]+/i },                                // color hex with $
-      { begin: /#[0-9a-f]{6}\b/i },                             // color hex with #
-      { begin: /-?\b\d+(\.\d+)?([eE][-+]?\d+)?/ }               // decimal + float + scientific
+      { begin: /\b0b[01]+/i },
+      { begin: /\b0x[\da-f]+/i },
+      { begin: /\$[0-9a-f]+/i },
+      { begin: /#[0-9a-f]{6}\b/i },
+      { begin: /-?\b\d+(\.\d+)?([eE][-+]?\d+)?/ }
     ],
     relevance: 0
   };
@@ -14,10 +14,15 @@ function hljsDefineGML(hljs) {
   const gmlStringMode = {
     className: 'string',
     variants: [
-      { begin: /@"/, end: /"/, contains: [] },                 // multiline string @" ... "
-      { begin: /\$"/, end: /"/, contains: [] },                // template string $" ... "
-      hljs.QUOTE_STRING_MODE                                   // normal "text"
+      { begin: /@"/, end: /"/, contains: [] },
+      { begin: /\$"/, end: /"/, contains: [] },
+      hljs.QUOTE_STRING_MODE
     ]
+  };
+
+  const gmlConstantMode = {
+    className: 'constant',
+    begin: /\b(?:true|false|undefined|noone|self|other|all|global|pi|GM_version|vk_right|c_red)\b/
   };
 
   return {
@@ -26,22 +31,21 @@ function hljsDefineGML(hljs) {
     keywords: {
       keyword:
         '#macro #region #endregion break case continue default do else enum exit for globalvar if repeat return switch until var while with and or not xor function',
-      literal:
-        'true false undefined noone self other all global local',
       built_in:
         'show_message show_debug_message instance_create_layer instance_destroy keyboard_check keyboard_check_pressed'
     },
     contains: [
-      hljs.C_LINE_COMMENT_MODE,                                // // comments
-      hljs.C_BLOCK_COMMENT_MODE,                               // /* ... */
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.C_BLOCK_COMMENT_MODE,
       {
-        className: 'doctag',                                   // /// doc-comments
+        className: 'doctag',
         begin: '///',
         end: '$',
         relevance: 10
       },
-      gmlStringMode,                                           // strings
-      gmlNumberMode                                            // numbers
+      gmlStringMode,
+      gmlNumberMode,
+      gmlConstantMode // <-- NEW
     ]
   };
 }
