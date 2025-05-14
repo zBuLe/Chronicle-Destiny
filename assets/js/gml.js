2583c3b0dbd26e0cdbf189ba31275b5ec9005bce
zBuLe <zbule@bluestar03.com>
2025-05-14 16:53:23 -0700

    style: refactor GML syntax highlighting modes for clarity and consistency

--- assets/js/gml.js
+++ assets/js/gml.js
@@ -2,11 +2,11 @@ function hljsDefineGML(hljs) {
   const gmlNumberMode = {
     className: 'number',
     variants: [
-      { begin: /\b0b[01]+/i },                                  // binary
-      { begin: /\b0x[\da-f]+/i },                              // hex
-      { begin: /\$[0-9a-f]+/i },                               // color hex with $
-      { begin: /#[0-9a-f]{6}\b/i },                            // color hex with #
-      { begin: /-?\b\d+(\.\d+)?([eE][-+]?\d+)?/ }              // decimal, float, scientific
+      { begin: /\b0b[01]+/i },
+      { begin: /\b0x[\da-f]+/i },
+      { begin: /\$[0-9a-f]+/i },
+      { begin: /#[0-9a-f]{6}\b/i },
+      { begin: /-?\b\d+(\.\d+)?([eE][-+]?\d+)?/ }
     ],
     relevance: 0
   };
@@ -14,23 +14,31 @@ function hljsDefineGML(hljs) {
   const gmlStringMode = {
     className: 'string',
     variants: [
-      { begin: /@"/, end: /"/, contains: [] },                // multiline string @" ... "
-      { begin: /\$"/, end: /"/, contains: [] },               // template string $" ... "
-      hljs.QUOTE_STRING_MODE                                   // normal "..."
+      { begin: /@"/, end: /"/, contains: [] },
+      { begin: /\$"/, end: /"/, contains: [] },
+      hljs.QUOTE_STRING_MODE
     ]
   };
 
   const gmlConstantMode = {
     className: 'constant',
     variants: [
-      // Core GML constants
+      // Core constants
       { begin: /\b(?:true|false|undefined|noone|self|other|all|global|pi|GM_version)\b/ },
-
-      // Keyboard constants (expand as needed)
+      // Keyboard constants
       { begin: /\b(?:vk_left|vk_right|vk_up|vk_down|vk_home|vk_end|vk_pageup|vk_pagedown|vk_space)\b/ },
-
-      // Color constants (expand as needed)
+      // Color constants
       { begin: /\b(?:c_red|c_blue|c_white|c_black|c_silver|c_lime|c_maroon|c_olive|c_navy)\b/ }
+    ]
+  };
+
+  const gmlBuiltinVariableMode = {
+    className: 'built_in',
+    variants: [
+      // Instance variables
+      { begin: /\b(?:x|y|object_index|image_index|speed|direction|gravity|friction|hspeed|vspeed|mask_index|path_index|path_position|path_speed|path_scale)\b/ },
+      // Argument variables
+      { begin: /\b(?:argument0|argument1|argument2|argument3|argument4|argument5|argument6|argument7|argument8|argument9)\b/ }
     ]
   };
 
@@ -44,17 +52,18 @@ function hljsDefineGML(hljs) {
         'show_message show_debug_message instance_create_layer instance_destroy keyboard_check keyboard_check_pressed'
     },
     contains: [
-      hljs.C_LINE_COMMENT_MODE,                                // // comments
-      hljs.C_BLOCK_COMMENT_MODE,                               // /* ... */
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.C_BLOCK_COMMENT_MODE,
       {
         className: 'doctag',
         begin: '///',
         end: '$',
         relevance: 10
       },
-      gmlStringMode,                                           // strings
-      gmlNumberMode,                                           // numbers
-      gmlConstantMode                                          // constants
+      gmlStringMode,
+      gmlNumberMode,
+      gmlConstantMode,
+      gmlBuiltinVariableMode
     ]
   };
 }
