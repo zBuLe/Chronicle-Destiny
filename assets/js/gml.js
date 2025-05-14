8a09f5ba66142919e73216c411e4eb0bb1f04dcc
zBuLe <zbule@bluestar03.com>
2025-05-14 15:21:19 -0700

    style: format GML language definition for improved readability and consistency

--- assets/js/gml.js
+++ assets/js/gml.js
@@ -3,33 +3,30 @@ function hljsDefineGML(hljs) {
     name: 'GML',
     aliases: ['gamemaker', 'gamemakerlanguage'],
     keywords: {
-      keyword: 'break case continue default do else enum exit for globalvar if repeat return switch until var while with and or not xor',
-      literal: 'true false undefined noone self other all global local',
-      built_in: 'show_message show_debug_message instance_create_layer instance_destroy keyboard_check keyboard_check_pressed'
+      keyword:
+        'break case continue default do else enum exit for globalvar if repeat return switch until var while with and or not xor',
+      literal:
+        'true false undefined noone self other all global local',
+      built_in:
+        'show_message show_debug_message instance_create_layer instance_destroy keyboard_check keyboard_check_pressed'
     },
     contains: [
-      hljs.C_LINE_COMMENT_MODE,         // // single line comments
-      hljs.C_BLOCK_COMMENT_MODE,        // /* multi-line comments */
+      // Single line comments //
+      hljs.C_LINE_COMMENT_MODE,
+      // Multi-line comments /* ... */
+      hljs.C_BLOCK_COMMENT_MODE,
+      // Doc comments (/// ...)
       {
         className: 'doctag',
-        begin: '///',                    // GML doc comments
+        begin: '///',
         end: '$',
         relevance: 10
       },
-      hljs.QUOTE_STRING_MODE,           // "strings"
-      hljs.APOS_STRING_MODE,            // 'strings'
-      hljs.C_NUMBER_MODE,               // numbers
-      {
-        className: 'function',
-        beginKeywords: 'function',      // optional, for custom additions
-        end: /[{;]/,
-        excludeEnd: true,
-        contains: [hljs.inherit(hljs.TITLE_MODE, {begin: /\w+/})]
-      },
-      {
-        className: 'symbol',
-        begin: /@[a-zA-Z_]\w*/          // optional: symbols like @myvar
-      }
+      // Strings "..." or '...'
+      hljs.QUOTE_STRING_MODE,
+      hljs.APOS_STRING_MODE,
+      // Numbers (decimal, hex)
+      hljs.C_NUMBER_MODE
     ]
   };
 }
