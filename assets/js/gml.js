3bd345a7bff0f8a68d929d566534d2f6bca67310
zBuLe <zbule@bluestar03.com>
2025-05-15 11:02:28 -0700

    style: enhance GML syntax highlighting with additional color variables and modes for improved clarity style: adjust root font size for better scaling in style.css fix: refine GML number and enum parsing in gml.js for better accuracy refactor: clean up gml.html structure for improved readability

--- assets/js/gml.js
+++ assets/js/gml.js
@@ -2,11 +2,11 @@ function hljsDefineGML(hljs) {
   const gmlNumberMode = {
     className: 'number',
     variants: [
-      { begin: /\b0b[01]+/i },
-      { begin: /\b0x[\da-f]+/i },
-      { begin: /\$[0-9a-f]+/i },
-      { begin: /#[0-9a-f]{6}\b/i },
-      { begin: /-?\b\d+(\.\d+)?([eE][-+]?\d+)?/ }
+      { begin: /0b[01]+/ },
+      { begin: /0x[0-9A-Fa-f]+/ },
+      { begin: /\$[0-9a-fA-F]+/ },
+      { begin: /#[0-9a-fA-F]{6}/ },
+      { begin: /-?\d+(\.\d+)?([eE][-+]?\d+)?/ }
     ],
     relevance: 0
   };
@@ -18,6 +18,17 @@ function hljsDefineGML(hljs) {
       { begin: /\$"/, end: /"/, contains: [] },
       hljs.QUOTE_STRING_MODE
     ]
+  };
+
+  // ✅ Enum fix: removed end + excludeEnd
+  const gmlEnumMode = {
+    beginKeywords: 'enum',
+    contains: [{ className: 'enum', begin: /\w+/ }]
+  };
+
+  const gmlEnumEntryMode = {
+    className: 'enumentry',
+    begin: /\b[A-Z_]{2,}\b/
   };
 
   const gmlConstantMode = {
@@ -32,74 +43,132 @@ function hljsDefineGML(hljs) {
   const gmlBuiltinVariableMode = {
     className: 'built_in',
     variants: [
-      { begin: /\b(?:x|y|object_index|image_index|speed|direction|gravity|friction|hspeed|vspeed|mask_index|path_index|path_position|path_speed|path_scale)\b/ },
-      { begin: /\b(?:argument0|argument1|argument2|argument3|argument4|argument5|argument6|argument7|argument8|argument9)\b/ }
+      { begin: /\b(?:x|y|object_index|image_index|speed|direction|gravity|friction|hspeed|vspeed|mask_index|path_index|path_position|path_speed|path_scale|sprite_index)\b/ },
+      { begin: /\bargument\d+\b/ }
     ]
   };
 
-  const gmlFunctionMode = {
-    className: 'function',
+  const gmlGlobalMode = {
+    className: 'global',
     variants: [
-      {
-        beginKeywords: 'function',
-        end: /\(/,
-        excludeEnd: true,
-        contains: [
-          hljs.inherit(hljs.TITLE_MODE, { begin: /[A-Za-z_]\w*/ })
-        ]
-      },
-      {
-        begin: /\b(?:keyboard_check|show_debug_message|show_message|instance_create_layer|instance_destroy|irandom_range)\b(?=\s*\()/,
-        relevance: 0
-      }
+      { begin: /(?<=\bglobal\.)\w+\b/ },
+      { begin: /(?<=\bglobalvar\s)\w+\b/ }
     ]
   };
 
-  const gmlEnumMode = {
-    beginKeywords: 'enum',
-    end: /[{;]/,
-    excludeEnd: true,
-    contains: [
-      {
-        className: 'enum',
-        begin: /\b[A-Za-z_]\w*\b/
-      }
-    ]
-  };
-
-  const gmlEnumEntryMode = {
-    className: 'enumentry',
+  const gmlLocalMode = {
+    className: 'local',
     variants: [
-      { begin: /\b[A-Za-z_]\w*(?=\s*=)/ },
-      { begin: /\.\s*\b[A-Za-z_]\w*\b/ }
+      { begin: /(?<=\bvar\s)\w+\b/ },
+      { begin: /\b_+\w+\b/ }
     ]
+  };
+
+  const gmlStaticMode = {
+    className: 'static',
+    begin: /(?<=\bstatic\s)\w+\b/
+  };
+
+  const gmlResourceMode = {
+    className: 'resource',
+    variants: [
+      { begin: /\b(?:obj_|spr_|bg_|rm_|fnt_|sh_|snd_|ts_|tm_)\w+\b/ },
+      { begin: /\b(?:o|spr|rm|fnt|snd)[A-Z]\w*\b/ }
+    ]
+  };
+
+  const gmlScriptMode = {
+    className: 'scriptname',
+    variants: [
+      { begin: /\b(?:scr_)\w+\b/ },
+      { begin: /\b(?:scr)[A-Z]\w*\b/ }
+    ]
+  };
+
+  const gmlBracesMode = {
+    className: 'symbol',
+    begin: /[()\[\]{}]/,
+    relevance: 0
+  };
+
+  const gmlOperatorMode = {
+    className: 'operator',
+    begin: /(\?\?=|\+\+|--|<<|>>|\+=|-=|\*=|\/=|==|!=|<=|>=|\?\?|\&\&|\|\||\^\^|[=<>+\-*/%!?~?:|&^])/,
+    relevance: 0
+  };
+
+  const gmlAccessorMode = {
+    className: 'operator',
+    begin: /(?<=\[)\s*[|?@#$]/,
+    relevance: 0
+  };
+
+  const gmlFunctionModes = [
+    {
+      className: 'function',
+      variants: [
+        { begin: /\b(?:show_message|show_debug_message|keyboard_check|keyboard_check_pressed|instance_create_layer|instance_destroy|irandom_range|string|array_create)\b/ }
+      ],
+      relevance: 0    // ✅ final polish
+    },
+    {
+      className: 'function',
+      beginKeywords: 'function',
+      end: /\(/,
+      excludeEnd: true,
+      contains: [hljs.inherit(hljs.TITLE_MODE, { begin: /\w+/ })]
+    }
+  ];
+
+  const gmlPreprocessorMode = {
+    className: 'keyword',
+    begin: /^#(?:macro|region|endregion)\b/,
+    relevance: 10
+  };
+
+  const gmlMacroMode = {
+    className: 'macro',
+    begin: /(?<=#macro\s)\w+\b/
+  };
+
+  const gmlKeywordMode = {
+    className: 'keyword',
+    begin: /\b(?:break|case|continue|default|do|else|enum|exit|for|globalvar|if|repeat|return|switch|until|var|while|with|and|or|not|xor|function|static)\b/
+  };
+
+  const gmlObjectVariableMode = {
+    className: 'objectvar',
+    begin: /\b[a-zA-Z_]\w*\b/,
+    relevance: 0
   };
 
   return {
     name: 'GML',
     aliases: ['gamemaker', 'gamemakerlanguage'],
-    keywords: {
-      keyword:
-        '#macro #region #endregion break case continue default do else enum exit for globalvar if repeat return switch until var while with and or not xor function',
-      literal:
-        'true false undefined noone self other all global local'
-    },
     contains: [
+      gmlPreprocessorMode,
+      gmlMacroMode,
       hljs.C_LINE_COMMENT_MODE,
       hljs.C_BLOCK_COMMENT_MODE,
-      {
-        className: 'doctag',
-        begin: '///',
-        end: '$',
-        relevance: 10
-      },
+      { className: 'doctag', begin: '///', end: '$', relevance: 10 },
       gmlEnumMode,
       gmlStringMode,
       gmlNumberMode,
+      gmlBracesMode,
+      gmlOperatorMode,
+      gmlAccessorMode,
+      gmlEnumEntryMode,
       gmlConstantMode,
       gmlBuiltinVariableMode,
-      gmlFunctionMode,
-      gmlEnumEntryMode
+      gmlGlobalMode,
+      gmlLocalMode,
+      gmlStaticMode,
+      gmlResourceMode,
+      gmlScriptMode,
+      gmlFunctionModes[0],
+      gmlFunctionModes[1],
+      gmlKeywordMode,
+      gmlObjectVariableMode
     ]
   };
 }
